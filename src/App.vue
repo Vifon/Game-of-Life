1b7a57e99b997d25866fa4572204b9cb4abaa866
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2019-03-15 19:56:00 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,201 @@
+<template>
+  <div>
+    <table class="centered">
+      <tr v-for="(_, y) in size">
+        <td v-for="(_, x) in size"
+            :class="{ alive: cells[idx(x,y)], dead: !cells[idx(x,y)] }"
+        ></td>
+      </tr>
+    </table>
+
+    <div class="controls">
+      <input id="edit" name="edit" type="button" value="Edit"
+             class="control"
+             v-if="!edit"
+             v-on:click="edit = true"
+      />
+    </div>
+    <div id="inputs" v-if="edit">
+      <div class="controls centered">
+        <input id="reseed" name="reseed" type="button" value="Reseed"
+               class="control"
+               v-on:click="reseed"
+        />
+        <condition-input ref="conditions" initial="23/3"
+                         class="control"
+        ></condition-input>
+      </div>
+      <table class="centered">
+        <tr v-for="(_, y) in size">
+          <td v-for="(_, x) in size"
+              :class="{ alive: seed[idx(x,y)], dead: !seed[idx(x,y)] }"
+              class="editable"
+              v-on:mousedown="flip('seed', x,y)"
+          ></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+ import ConditionInput from './components/ConditionInput.vue'
+
+ export default {
+   name: 'app',
+   components: {
+     ConditionInput
+   },
+   data: function () {
+     return {
+       seed: undefined,
+       size: undefined,
+       edit: false,
+       turnTime: 200,
+       timer: undefined,
+       cells: undefined,
+       textSeed: `
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ x _ _ _ x _ _ _ _ _ _
+         _ _ _ _ _ _ x x x x x _ _ _ _ _ _
+         _ _ _ _ _ _ _ x x x _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ x _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ x _ _ _ x _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+       `
+     }
+   },
+   methods: {
+     idx: function (x, y) {
+       return y * this.size + x;
+     },
+     flip: function (array, x, y) {
+       var idx = this.idx(x,y);
+       this[array][idx] = !this[array][idx];
+     },
+     countNeightbors: function (x, y) {
+       var neighbors = 0;
+       for (var dy = -1; dy <= 1; ++dy) {
+         for (var dx = -1; dx <= 1; ++dx) {
+           var nx = x + dx;
+           var ny = y + dy;
+           if (nx >= 0 && nx < this.size &&
+               ny >= 0 && ny < this.size &&
+               (dx != 0 || dy != 0)) {
+             if (this.cells[this.idx(nx,ny)]) {
+              ++neighbors;
+             }
+           }
+         }
+       }
+       return neighbors;
+     },
+     alive: function (x, y) {
+       var neighbors = this.countNeightbors(x,y)
+       if (this.cells[this.idx(x,y)]) {
+         /* Survival */
+         return this.survivalCondition.includes(neighbors);
+       } else {
+         /* Birth */
+         return this.birthCondition.includes(neighbors);
+       }
+     },
+     nextTurn: function () {
+       var nextCells = new Array(this.size2);
+       for (var y = 0; y < this.size; ++y) {
+         for (var x = 0; x < this.size; ++x) {
+           nextCells[this.idx(x,y)] = this.alive(x,y);
+         }
+       }
+       this.cells = nextCells;
+       this.timer = setTimeout(() => this.nextTurn(), this.turnTime);
+     },
+     reseed: function () {
+       if (this.timer) {
+         clearTimeout(this.timer);
+       }
+
+       if (!this.seed) {
+         this.seed = this.textSeed.split('')
+                         .filter(cell =>
+                           cell == '_' || cell == 'x')
+                         .map(cell => cell == 'x');
+       }
+       this.size = Math.sqrt(this.seed.length);
+
+       this.cells = this.seed;
+       this.timer = setTimeout(() => this.nextTurn(), 1000);
+     }
+   },
+   mounted: function () {
+     this.reseed();
+   },
+   computed: {
+     size2: function () {
+       return this.size * this.size;
+     },
+     survivalCondition: function () {
+       if (this.edit) {
+         return this.$refs.conditions.value.survival;
+       } else {
+         return [2, 3];
+       }
+     },
+     birthCondition: function () {
+       if (this.edit) {
+         return this.$refs.conditions.value.birth;
+       } else {
+         return [3];
+       }
+     }
+   }
+ }
+</script>
+
+<style>
+ #app {
+   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   margin-top: 60px;
+ }
+ .block {
+   display: block;
+ }
+ .centered {
+   margin-left: auto;
+   margin-right: auto;
+ }
+ .controls {
+   display: flex;
+   justify-content: center;
+ }
+ .control {
+   margin: 2px;
+ }
+ td {
+   min-width: 20px;
+   width: 20px;
+   height: 20px;
+   border: 1px #eee solid;
+ }
+ .alive {
+   background-color: black;
+ }
+ .dead {
+   background-color: white;
+ }
+ .editable {
+   opacity: 0.7;
+ }
+</style>
---
+++ src/components/ConditionInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="condition-input">
+    <input type="text" v-model="raw_value" size=20>
+  </div>
+</template>
+
+<script>
+ export default {
+   name: 'ConditionInput',
+   props: {
+     initial: String
+   },
+   data: function () {
+     return {
+       raw_value: this.initial
+     };
+   },
+   computed: {
+     value: function () {
+       var stringToInts = (s) => s.split('').map(x => parseInt(x));
+
+       var s = this.raw_value;
+       var pos = s.indexOf('/');
+
+       var [survival, birth] =
+         [s.slice(0, pos), s.slice(pos+1)].map(stringToInts);
+       return {
+         survival: survival,
+         birth: birth
+       };
+     }
+   }
+ }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
