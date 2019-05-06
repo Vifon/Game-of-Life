4990025b7e938c426f354886091927071bc4a3f1
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2019-05-06 17:09:42 +0200

    Make deploy.sh more robust

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,14 @@
 
 set -o errexit -o nounset -o pipefail
 
+git stash -k
+git stash apply --index
+
 npm run build
 
 git add -f dist/
 git commit -m Deploy
 git push origin "$(git subtree split --prefix dist/ master)":gh-pages --force
 git reset --hard '@{1}'
+
+git stash pop --index
