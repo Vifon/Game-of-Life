1f60b569a913ee957b16784b3bb5c92ac6e6d947
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2019-05-30 14:11:41 +0200

    Deploy

---
+++ precache-manifest.46498633fdbd9b3c8d7a4d7f4a20ca2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "10a3b7f234a5d9186944",
+    "url": "js/chunk-vendors.89bf7c00.js"
+  },
+  {
+    "revision": "3669949f9ed975f0a689",
+    "url": "js/app.c757f519.js"
+  },
+  {
+    "revision": "4eb1e5754896bd40cd2e65d572088a0f",
+    "url": "index.html"
+  },
+  {
+    "revision": "3669949f9ed975f0a689",
+    "url": "css/app.c3f83618.css"
+  }
+];
